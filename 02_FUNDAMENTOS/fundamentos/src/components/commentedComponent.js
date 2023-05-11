775bcc26e97c679029e1237893b73d57f901c2ed
William Radi <william.radi@tray.intranet>
2023-05-10 23:36:03 -0300

    Document: adiciona linhas no commentedComponents

--- 02_FUNDAMENTOS/fundamentos/src/components/commentedComponent.js
+++ 02_FUNDAMENTOS/fundamentos/src/components/commentedComponent.js
@@ -1,10 +1,13 @@
 const CommentedComponent = () => {
+  const paragfrafo = 'Este parágrafo está num template expression'
   // JSX sempre vai estar no return, é o resultado do meu componente
   return (
     // deve haver sempre uma tag encapsulando o jsx
     <div>
       {/* alguns atribus do JSX devem ser diferente do html pra não dar conflito (tipo o className == class) */}
       <p className="paragrafo_teste">Este é um parágrafo no componente comentado</p>
+      {/* TUDO que executando no JSX entre chaves, vira código JS */}
+      <p>{paragfrafo}</p>
     </div>
   )
 }
