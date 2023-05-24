e92e990a94c2098451310b590209da3f70d56a1f
William Radi <william.radi@tray.intranet>
2023-05-23 21:58:11 -0300

    Imagens

--- 03_AVANCANDO/avancando/src/App.js
+++ 03_AVANCANDO/avancando/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import Coala from './assets/coala.jpg'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Seção 3: Avançando com React</h1>
+      <h3>Esta imagem está em /public</h3>
+      <div>
+        <img src="/alpaca.jpg" alt="alpaca" />
+        {/* Imagem em public = basta passar /nome_do_arquivo.jpg */}
+      </div>
+      <h3>Esta imagem está em src</h3>
+        <img src={Coala} alt="coala" />
+      <div>
+
+      </div>
     </div>
   );
 }
