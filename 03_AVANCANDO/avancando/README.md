17060a435ea105b0a6f97972fd6dc47585925cfb
William Radi <william.radi@tray.intranet>
2023-05-25 08:42:54 -0300

    LIstagem usando key

--- 03_AVANCANDO/avancando/README.md
+++ 03_AVANCANDO/avancando/README.md
@@ -6,3 +6,9 @@ Geral:
   * São funções
   * Servem para guardar e alterar estado de algum dado
   * Começa com `use` (useState)
+
+## Map
+
+* Sempre precisa usar a propriedade `key`
+  * Em último caso, qdo n houver ID, usar o index do map mesmo
+
--- 03_AVANCANDO/avancando/src/components/ListRender.js
+++ 03_AVANCANDO/avancando/src/components/ListRender.js
@@ -2,12 +2,24 @@ import { useState } from "react";
 
 const ListRender = () => {
   const [list] = useState(['Tobias', 'Matias', 'Peixe boi'])
+  const [users] = useState([
+    {id: 1, name: 'Tobias'},
+    {id: 2, name: 'Matias'},
+    {id: 3, name: 'Peixe boi'}
+  ])
 
   return (
     <div>
+      <p>Lista sem ID</p>
       <ul>
-        {list.map((item) => (
-          <li>{item}</li>
+        {list.map((item, i) => (
+          <li key={i}>{item}</li>
+        ))}
+      </ul>
+      <p>Lista com ID</p>
+      <ul>
+        {users.map(user => (
+          <li key={user.id}>{user.name}</li>
         ))}
       </ul>
     </div>
