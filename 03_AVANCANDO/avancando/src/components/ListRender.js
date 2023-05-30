46a6e711ef43ecbdfeee46cfa1e2f01e26ecba61
William Radi <william.radi@tray.intranet>
2023-05-29 22:31:31 -0300

    previous state

--- 03_AVANCANDO/avancando/src/components/ListRender.js
+++ 03_AVANCANDO/avancando/src/components/ListRender.js
@@ -2,11 +2,23 @@ import { useState } from "react";
 
 const ListRender = () => {
   const [list] = useState(['Tobias', 'Matias', 'Peixe boi'])
-  const [users] = useState([
+  const [users, setUsers] = useState([
     {id: 1, name: 'Tobias'},
     {id: 2, name: 'Matias'},
     {id: 3, name: 'Peixe boi'}
   ])
+
+  const deleteRandom = () => {
+    const randomNumber = Math.floor(Math.random() * 4)
+    // setUsers((prevUsers) => {
+    //   return prevUsers.filter((userDoPrevUser) => randomNumber !== userDoPrevUser.id)
+    // })
+
+    // Pq consigo usar o previous state sem passar argumento pro set?
+    setUsers(() => {
+      return users.filter((userDoPrevUser) => randomNumber !== userDoPrevUser.id)
+    })
+  }
 
   return (
     <div>
@@ -22,6 +34,7 @@ const ListRender = () => {
           <li key={user.id}>{user.name}</li>
         ))}
       </ul>
+      <button onClick={deleteRandom}>Delete random user</button>
     </div>
   )
 }
