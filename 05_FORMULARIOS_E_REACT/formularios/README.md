fefe92e47dc7bf6e2c50e476466bfbd3b50ae45b
William Radi <william.radi@tray.intranet>
2023-06-18 00:27:25 -0300

    manipulando dados do formulário

--- 05_FORMULARIOS_E_REACT/formularios/README.md
+++ 05_FORMULARIOS_E_REACT/formularios/README.md
@@ -1,9 +1,18 @@
 # React e formulários
 
-## Consulta básica
+## Criação de campos
 
 * Atrbuto `labelFor`, da tag `label`, deve bater com o valor da propriedade `name`, da tag `input`
+* Prática mais comum: Usar label envolvendo o input
 
-## Boas práticas / Práticas comuns
+## Manipulação de dados inputados
 
-* Usar label envolvendo o input
+* Modo 1:
+  * usa-se useState
+  * no input, coloca-se o evento onChange e chama o handle da referente ao input (handleName)
+  * handle é declarado recebendo um evento como parâmetro.
+  * valor do que foi digitado está em `evento.target.value`
+
+* Modo 2:
+  * Mesmo esquema do modo 1, mas passando a função do onChange inline
+  * função "handle" se torna desnecessária
--- 05_FORMULARIOS_E_REACT/formularios/src/components/MyForm.js
+++ 05_FORMULARIOS_E_REACT/formularios/src/components/MyForm.js
@@ -1,18 +1,28 @@
 import './MyForm.css'
-
+import { useState } from 'react'
 const MyForm = () => {
+  const [name, setName] = useState()
+  const [email, setEmail] = useState()
+
+  const handleName = (e) => {
+    setName(e.target.value)
+  }
+
+  console.log(name);
+  console.log(email);
+
   return (
     <div>
       <form>
         <div>
           <label htmlFor="name">Nome:</label>
-          <input type="text" name="name" placeholder="Digite seu nome..." />
+          <input type="text" name="name" placeholder="Digite seu nome..."  onChange={handleName} />
         </div>
         {/* uso mais comum: tag label envolvendo o input */}
         <label>
           {/* span só é usado pra estilização */}
           <span>Email:</span>
-          <input type="email" name="email" placeholder="Digite seu e-mail..." />
+          <input type="email" name="email" placeholder="Digite seu e-mail..." onChange={(e) => setEmail(e.target.value)} />
         </label>
         <input type="submit" value="Enviar" />
       </form>
